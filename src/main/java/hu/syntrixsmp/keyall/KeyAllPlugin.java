9e7740b1c865d46beee937e4f6c36640d5ccefc0
nmsenki2-cloud <nmsenki2@gmail.com>
2026-05-10 01:12:49 +0200

    Update KeyAllPlugin.java

--- src/main/java/hu/syntrixsmp/keyall/KeyAllPlugin.java
+++ src/main/java/hu/syntrixsmp/keyall/KeyAllPlugin.java
@@ -1,22 +1,17 @@
 package hu.syntrixsmp.keyall;
-
 import org.bukkit.plugin.java.JavaPlugin;
-
 public class KeyAllPlugin extends JavaPlugin {
-
     private static KeyAllPlugin instance;
     private KeyAllManager manager;
-
     @Override
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
-
         manager = new KeyAllManager(this);
-
+        // Automatikus indítás
+        manager.autoStart();
         // Parancs regisztrálása
         getCommand("keyall").setExecutor(new KeyAllCommand(this));
-
         // PlaceholderAPI hook
         if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
             new KeyAllPlaceholder(this).register();
@@ -24,10 +19,8 @@ public class KeyAllPlugin extends JavaPlugin {
         } else {
             getLogger().warning("[KeyAll] PlaceholderAPI nem található! A placeholder nem fog működni.");
         }
-
         getLogger().info("[KeyAll] Plugin sikeresen betöltve!");
     }
-
     @Override
     public void onDisable() {
         if (manager != null) {
@@ -35,11 +28,9 @@ public class KeyAllPlugin extends JavaPlugin {
         }
         getLogger().info("[KeyAll] Plugin leállítva.");
     }
-
     public static KeyAllPlugin getInstance() {
         return instance;
     }
-
     public KeyAllManager getManager() {
         return manager;
     }
